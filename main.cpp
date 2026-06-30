5b76a71d74dfb72f363edb7045f15379ff55e983
MARIA EDUARDA SILVA MARMENTINI <maria_e_marmentini@estudante.sesisenai.org.br>
2026-06-30 15:50:01 -0300

    adicionei personagem chef Mei

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+// compilador precisa ver a struct antes de usar
+struct DadosPersonagem {
+    string nome;
+    int vida;
+    int ataque;
+};
+
+// compilador precisa ver a função antes de usar
+DadosPersonagem exibir(string nome, int vida, int ataque) {
+    DadosPersonagem dados;
+    dados.nome = "!! " + nome + " !!";
+    dados.vida = vida;
+    dados.ataque = ataque;
+    return dados; 
+}
 
 int main() {
-    std::cout << "Hanamori !! :3" << std::endl;
+    cout << "Hanamori !! :3" << endl;
+    cout << "-------------------------\n";
+
+    // Chamada da função
+    DadosPersonagem chef = exibir("Mei, a Mestre dos Macarons", 5, 15);
+
+    // Exibindo os dados na tela
+    cout << chef.nome << "\n";
+    cout << " Vida: " << chef.vida << "\n";
+    cout << " Ataque: " << chef.ataque << "\n";
+    cout << "-------------------------\n";
+    
     return 0;
 }
